bf7033d78f08ab3df401b9c37859684d34816fe2
mathew nuval <mathew.nuval.339@my.csun.edu>
2020-03-19 12:10:59 -0700

    Implemented Client and Server in separate Project folders

---
+++ sample.client/src/main/java/Driver.java
@@ -0,0 +1,7 @@
+import jdk.internal.ref.Cleaner;
+
+public class Driver {
+    public static void main(String args[]){
+        Client client = new Client("127.0.0.1",5000);
+    }
+}
---
+++ sample.server/src/main/java/Driver.java
@@ -0,0 +1,6 @@
+public class Driver {
+    public static void main(String args[]) {
+        Server server = new Server(5000);
+
+    }
+}
