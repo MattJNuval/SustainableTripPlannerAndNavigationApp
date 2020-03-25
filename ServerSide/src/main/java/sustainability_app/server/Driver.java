0d3c78b7bc15e724e0674393c4c9ae800ee6b31e
cshadd <christian.v.t.shadd@gmail.com>
2020-03-25 14:40:40 -0700

    Added comments to Driver class

--- ServerSide/src/main/java/sustainability_app/server/Driver.java
+++ ServerSide/src/main/java/sustainability_app/server/Driver.java
@@ -5,6 +5,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import sustainability_app.server.comm.ServerClientCommunication;
 
+/**
+ * Driver for main entry.
+ * @author Christian
+ *
+ */
 public class Driver {
     public final static String AIR_VISUAL_API_KEY =
             "d7664ac9-d9fb-4ed4-b6f6-2e8feac28693";
@@ -15,10 +20,13 @@ public class Driver {
     public final static Logger LOGGER =
             Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     
-    private ServerClientCommunication serverClientCommunication;
-    
     private Driver() { }
     
+    /**
+     * Main entry point. This requires one single argument for the port number in which the server must run.
+     * @param args arguments to be inputted.
+     * @throws IllegalArgumentException if no arguments are inputted.
+     */
     public static void main(final String[] args) throws IllegalArgumentException {
         if (args.length >= 1) {
             final int portNumber = Integer.parseInt(args[0]);
@@ -26,18 +34,19 @@ public class Driver {
                     + "App Server starting on port " + portNumber + ".");
             
             final Driver driver = new Driver();
+            ServerClientCommunication serverClientCommunication = null;
             
             try {
-                driver.serverClientCommunication = new ServerClientCommunication(portNumber);
+                serverClientCommunication = new ServerClientCommunication(portNumber);
                 LOGGER.log(Level.INFO, "Ready");
                 while (true) {
-                    driver.serverClientCommunication.listen();
+                    serverClientCommunication.listen();
                 }
             } catch (IOException e) {
                 LOGGER.log(Level.SEVERE, "Server error.", e);
             } finally {
                 try {
-                    driver.serverClientCommunication.close();
+                    serverClientCommunication.close();
                 } catch (IOException e) {
                     LOGGER.log(Level.SEVERE, "Could not close server.", e);
                 }
